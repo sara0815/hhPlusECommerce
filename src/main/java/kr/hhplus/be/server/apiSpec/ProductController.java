d217d0a57e4a91c5338296935a2c3f01ddc72b5c
sara0815 <sara815@nate.com>
2025-08-01 05:13:28 +0900

    상품 기존 기능 수정

--- src/main/java/kr/hhplus/be/server/apiSpec/ProductController.java
+++ src/main/java/kr/hhplus/be/server/apiSpec/ProductController.java
@@ -1,44 +1,44 @@
-package kr.hhplus.be.server.apiSpec;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import kr.hhplus.be.server.product.entity.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-@RestController
-@RequestMapping("/product")
-public class ProductController {
-    @Operation(summary = "상품 조회")
-    @GetMapping("/{id}")
-    public Product getProduct(
-            @Parameter(name="id", description = "상품 id")
-            @PathVariable long id
-    ) {
-        return new Product(id, "상품1", 10000, 1000, null, new Date());
-    }
-
-    @Operation(summary = "상품 리스트")
-    @GetMapping("/")
-    public List<Product> getProductList(
-    ) {
-        Product product = new Product(1, "상품1", 10000, 1000, null, new Date());
-        List<Product> productList = new ArrayList<>();
-        productList.add(product);
-        return productList;
-    }
-
-    @Operation(summary = "best top5 상품 조회", description = "3일간 top5 베스트 상품 조회")
-    @GetMapping("/best")
-    public List<Product> getBestProduct(@PathVariable long id) {
-        List<Product> productList = new ArrayList<>();
-        Product product = new Product(id, "상품1", 10000, 1000, null, new Date());
-        productList.add(product);
-        return productList;
-    }
-}
+//package kr.hhplus.be.server.apiSpec;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.Parameter;
+//import kr.hhplus.be.server.product.entity.Product;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/product")
+//public class ProductController {
+//    @Operation(summary = "상품 조회")
+//    @GetMapping("/{id}")
+//    public Product getProduct(
+//            @Parameter(name="id", description = "상품 id")
+//            @PathVariable long id
+//    ) {
+//        return new Product(id, "상품1", 10000, 1000, null, new Date());
+//    }
+//
+//    @Operation(summary = "상품 리스트")
+//    @GetMapping("/")
+//    public List<Product> getProductList(
+//    ) {
+//        Product product = new Product(1, "상품1", 10000, 1000, null, new Date());
+//        List<Product> productList = new ArrayList<>();
+//        productList.add(product);
+//        return productList;
+//    }
+//
+//    @Operation(summary = "best top5 상품 조회", description = "3일간 top5 베스트 상품 조회")
+//    @GetMapping("/best")
+//    public List<Product> getBestProduct(@PathVariable long id) {
+//        List<Product> productList = new ArrayList<>();
+//        Product product = new Product(id, "상품1", 10000, 1000, null, new Date());
+//        productList.add(product);
+//        return productList;
+//    }
+//}
